b57a9f7300bd1b3b9e79cd9317872b719e4a7941
ori <mr.kuyus@yahoo.com>
2013-07-11 17:16:29 +0700

    finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+  describe "Home page" do
+    
+    it "should have the h1 'Sample'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample')
+      end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample | Home")
+      end
+  end
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text  => "Ruby on Rails Tutorial Sample | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+      end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample | About Us")
+    end
+  end
+end
